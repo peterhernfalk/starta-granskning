f5b15b776ca4587280d745446b2d1a6adfa3bc04
Peter Hernfalk <peter.hernfalk@callistaenterprise.se>
2021-12-26 10:06:46 +0100

    Förenkling av Javascriptkod samt komplettering av readme

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ Denna tjänst är till för att underlätta start av I- eller T-granskningstjän
 Det åstadkoms främst genom att domännamn och domänversion väljs från listboxar istället för att skrivas in som url-parametrar till granskningstjänsterna.
 
 ## Teknisk information om tjänsten
-- Denna tjänst är utvecklad i HTML, Javascript och CSS
+- Denna tjänst är utvecklad i HTML, Javascript och (snart även) CSS
 - Listboxen med domännamn populeras genom att Javascriptkod hämtar innehållet från rivta-domains på Bitbucket
 - Listboxen med domänversioner populeras genom att Javascriptkod hämtar vald domäns befintliga taggar från domänens repo i Bitbucket
+
+### Hämtning av listor med domännamn respektive taggnamn
+- Javascriptfunktionen initDomainNameList() hämtar domännamn och fyller comboboxlistan med dem
+- Domännamnen hämtas från ett Bitbucket med hjälp av Bitbuckets API, version 2
+  - URL till den Bitbucketsida som innehåller domännamnen: https://api.bitbucket.org/2.0/repositories/rivta-domains/?pagelen=100&max_depth=10&sort=name
+- Domännamnen hämtas i JSON-format. Från JSON-objekten hämtas domännamn samt url till varje domäns Bitbucketsida där dess taggar finns angivna
+  - Domännamn och url till domänens sida med taggar sparas i ett key/value-objekt
+- När användare väljer en domän så anropas funktionen initDomainTagList()
+  - Funktionen hämtar url till vald domäns sida med taggar från key/value-objektet
+  - Därefter hämtas en lista i JSON-format med befintliga taggar, vilka sedan populeras i dess comboboxlista
--- javascript/service_logic.js
+++ javascript/service_logic.js
@@ -7,21 +7,16 @@ async function initDomainNameList() {
     select.innerHTML = "";
 
     var repo_listing = "https://api.bitbucket.org/2.0/repositories/rivta-domains/?pagelen=100&max_depth=10&sort=name"
-
     const response = await fetch(repo_listing);
     const rivta_domains = await response.json();
-    //console.log("rivta_domains:");
-    //console.log(rivta_domains);
 
-    jsonAdd2domainNames("");
+    domainNames.push("");
     for (let i = 0; i < rivta_domains.values.length; i++) {
         if (rivta_domains.values[i].name.startsWith("riv") == true) {
-            jsonAdd2domainNames(rivta_domains.values[i].name);
+            domainNames.push(rivta_domains.values[i].name);
             domain_tag_dictionary[ rivta_domains.values[i].name ] = rivta_domains.values[i].links.tags.href;
         }
     }
-    //console.log("initDomainNameList.domain_tag_dictionary: " + domain_tag_dictionary);
-    //console.log(domain_tag_dictionary);
 
     for (let i = 0; i < domainNames.length; i++) {
         var option_element = document.createElement('option');
@@ -46,9 +41,9 @@ async function initDomainTagList() {
         select.removeChild(select.childNodes[0]);
     }
 
-    jsonAdd2domainTags("");
+    domainTags.push("");
     for (let i = 0; i < domain_tags.values.length; i++) {
-        jsonAdd2domainTags(domain_tags.values[i].name);
+        domainTags.push(domain_tags.values[i].name);
     }
     for (let i = 0; i < domainTags.length; i++) {
         var option_element = document.createElement('option');
@@ -59,39 +54,6 @@ async function initDomainTagList() {
 
     document.getElementById("select_version").style.display = "block";
 }
-
-
-function fetchDomainTagList(domainName) {
-    const domainTags = [];
-
-    //--- Hard coded values
-    if (domainName == "riv.clinicalprocess.activity.request") {
-        domainTags[0] = "1.0.2";
-    } else if (domainName == "riv.clinicalprocess.healthcond.certificate") {
-        domainTags[0] = "4.0.5";
-    }
-
-    //console.log("fetchDomainTagList:\n" + domain_tag_dictionary);
-
-
-    //--- 2do: Fetch domain names from Bitbucket
-    //Source: tags in dropdown list in a commit page for a domain, such as: https://bitbucket.org/rivta-domains/riv.clinicalprocess.healthcond.description/commits/
-    //Better source: find "/tags/" in https://api.bitbucket.org/2.0/repositories/rivta-domains/riv.itintegration.engagementindex/refs/tags
-    // The page link is derived from https://api.bitbucket.org/2.0/repositories/rivta-domains/?pagelen=100&max_depth=10
-    // search for: "tags": {"href": and open link
-    // Search for: "commits": {"href": which will find all tags
-
-    return domainTags;
-}
-
-function jsonAdd2domainNames(domainName) {
-    domainNames.push(domainName);
-}
-
-function jsonAdd2domainTags(domainTag) {
-    domainTags.push(domainTag);
-}
-
 
 function startIgranskning() {
     var domain = document.getElementById("domain_name_list").value
